ffb8fdda4046c1c8b88f7018a7485580b29ad1ab
takuma-ru <kcattdev@gmail.com>
2022-01-24 16:04:24 +0900

    [Add] setup

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ events }}
+    <button @click="getEvents()">
+      get events
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      events: null,
+    };
+  },
+  methods: {
+    async getEvents() {
+      this.events = await this.$supabase.from("profiles").select("*");
+    },
+  },
+};
+</script>
