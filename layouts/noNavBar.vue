f0d49cc172931e1e49a06a704114e86c36560885
takuma-ru <kcattdev@gmail.com>
2022-11-28 20:56:33 +0900

    [Add] noNavBar layout

---
+++ layouts/noNavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="default-layout">
+    <div class="contents">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+/* -- type, interface -- */
+
+/* -- store -- */
+
+/* -- props, emit -- */
+
+/* -- variable(ref, reactive, computed) -- */
+
+/* -- function -- */
+
+/* -- watch -- */
+
+/* -- life cycle -- */
+</script>
+
+<style lang="scss" scoped>
+#default-layout {
+  display: grid;
+  grid-template-columns: 1fr;
+
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  .contents {
+    padding: 1em;
+  }
+}
+</style>
