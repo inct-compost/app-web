32bbc2b9f1e299015780fee450971743bd6afc09
takuma-ru <kcattdev@gmail.com>
2022-11-30 10:22:05 +0900

    [Add] HeaderTitleList util

---
+++ utils/utils/headerTitleList.ts
@@ -0,0 +1,35 @@
+import { IconNameType } from '~/types/icon/IconNameType'
+
+interface IHeaderTitle {
+  path: string
+  icon?: IconNameType
+  title: string
+}
+
+export const headerTitle = () => {
+  const headetTitleList = ref<Array<IHeaderTitle>>([
+    {
+      path: '/',
+      title: '○○のコンポスト'
+    },
+    {
+      path: '/dashboard',
+      icon: 'dashboard',
+      title: 'ダッシュボード'
+    },
+    {
+      path: '/setting',
+      icon: 'settings',
+      title: '設定'
+    }
+  ])
+
+  const currentHeaderTitle = computed(() => {
+    return headetTitleList.value.filter(headerTitle => headerTitle.path === useRoute().fullPath)[0]
+  })
+
+  return {
+    headetTitleList,
+    currentHeaderTitle
+  }
+}
